0734fd1a51cee9c03c5662892147602e034d9271
bronsong <59542283+bronsong@users.noreply.github.com>
2022-04-08 22:41:31 -0400

    Teleport Manager Updated
    Objects should now be teleported in a grid rather than a diagonal line.

--- Assets/HideAndSeek/Scripts/Bronson/TeleportManager.cs
+++ Assets/HideAndSeek/Scripts/Bronson/TeleportManager.cs
@@ -11,8 +11,8 @@ public class TeleportManager : MonoBehaviour
     public void TeleportObjectsToArea()
     {
         TeleportArea area = teleportAreas[teleportLocation];    //Retrieve the current teleport destination
-        float xSeparation = (area.maximumLocation.x - area.minimumLocation.x) / objectsToTeleport.Count;
-        float ySeparation = (area.maximumLocation.y - area.minimumLocation.y) / objectsToTeleport.Count;
+        float xSeparation = (area.maximumLocation.x - area.minimumLocation.x) / area.separationDistance;
+        float ySeparation = (area.maximumLocation.y - area.minimumLocation.y) / area.separationDistance;
         float currentX = area.minimumLocation.x;
         float currentY = area.maximumLocation.y;
         foreach(Transform obj in objectsToTeleport){
@@ -20,7 +20,15 @@ public class TeleportManager : MonoBehaviour
             obj.position = newLoc;
             obj.gameObject.SendMessage(area.teleportMessage);
             currentX += xSeparation;
-            currentY += ySeparation;
+            if(currentX > area.maximumLocation.x)
+            {
+                currentX = area.minimumLocation.x;
+                currentY += ySeparation;
+                if(currentY > area.maximumLocation.y)
+                {
+                    Debug.LogWarning("Object teleported outside of the specified area.  Please reduce the separation distance or define a larger area.");
+                }
+            }
         }
     }
 
@@ -30,6 +38,7 @@ public class TeleportManager : MonoBehaviour
         [Tooltip("The minimum location in worldspace to send an object.")] public Vector2 minimumLocation = new Vector2(-1f,-1f);
         [Tooltip("The maximum location in worldspace to send an object.")] public Vector2 maximumLocation = new Vector2(1,1);
         [Tooltip("The height in world coordinates at which to send an object.")] public float zLocation = 0.1f;
+        [Tooltip("The distance to separate the teleported objects.")] public float separationDistance = 1.0f;
         [Tooltip("Sends this message (if provided) to a teleported object after it reaches its destination.")] public string teleportMessage;
     }
 }
