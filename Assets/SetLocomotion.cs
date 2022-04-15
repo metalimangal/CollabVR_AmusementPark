06cbdf6d98dc0e7ac43a51d506f117320cbb681a
mins-verse <87890982+mins-verse@users.noreply.github.com>
2022-04-14 21:08:33 -0400

    Change#1

--- Assets/SetLocomotion.cs
+++ Assets/SetLocomotion.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Unity.XR.CoreUtils;
+using Photon.Pun;
+
 
 public class SetLocomotion : MonoBehaviour
 {
     private Transform target;
+    private PhotonView pv;
 
     // Start is called before the first frame update
     void Start()
     {
         target = transform.parent.GetChild(0);
+        pv = this.GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position;
-        transform.rotation = target.rotation;
+        if (pv.IsMine)
+        {
+            transform.position = target.position;
+            transform.rotation = target.rotation;
+        }
     }
 }
