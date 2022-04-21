6c307a21f51fb2f91087758986a4b08ac6d13b9e
mins-verse <87890982+mins-verse@users.noreply.github.com>
2022-04-21 19:48:26 -0400

    Add Audio Source and Effect Sound
    Add Audio Source and Effect Sound

--- Assets/RespawnBall.cs
+++ Assets/RespawnBall.cs
@@ -7,10 +7,12 @@ using Photon.Realtime;
 
 public class RespawnBall : MonoBehaviour
 {
+    private AudioSource audEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audEffect = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,7 @@ public class RespawnBall : MonoBehaviour
     {
         if (other.tag == "BasketBall")
         {
+            audEffect.PlayOneShot(audEffect.clip);
             other.transform.position = transform.position + new Vector3(0.0f, -0.5f, 0.0f);
             PhotonView pv = other.gameObject.GetComponent<PhotonView>();
             Debug.Log(pv.OwnerActorNr + " : Get Score");
