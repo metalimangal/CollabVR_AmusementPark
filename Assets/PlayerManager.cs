738d09b57c7b475b80a4817e94843651232a6e71
PedroAcevedo <pdacevedo@uninorte.edu.co>
2022-04-21 20:27:45 -0400

    Adding avatar color identification

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -11,13 +11,20 @@ namespace Com.MyCompany.MyGame
     /// </summary>
     public class PlayerManager : MonoBehaviourPunCallbacks
     {
-        
-        # region Public Fields
+
+        #region Public Fields
 
         [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
         public static GameObject LocalPlayerInstance;
 
         #endregion
+
+        #region Private Fields
+
+        private string[] userColor = { "Red",  "Blue", "Green", "White", "Black" };
+
+        #endregion
+
 
         #region MonoBehaviour CallBacks
 
@@ -31,6 +38,11 @@ namespace Com.MyCompany.MyGame
             if (photonView.IsMine)
             {
                 PlayerManager.LocalPlayerInstance = this.gameObject;
+
+                int characterColor = (int)Random.Range(0.0f, userColor.Length);
+
+                set_skinned_mat("LeftHand Controller", 0, Resources.Load("Materials/" + userColor[characterColor], typeof(Material)) as Material);
+                set_skinned_mat("RightHand Controller", 0, Resources.Load("Materials/" + userColor[characterColor], typeof(Material)) as Material);
                 //transform.GetChild(1).GetComponent<OVRManager>().enabled = true;
             }
 
@@ -62,6 +74,19 @@ namespace Com.MyCompany.MyGame
         void ProcessInputs()
         {
 
+        }
+
+        void set_skinned_mat(string obj_name, int Mat_Nr, Material Mat)
+        {
+            GameObject obj = GameObject.Find(obj_name);
+
+            SkinnedMeshRenderer renderer = obj.GetComponentInChildren<SkinnedMeshRenderer>();
+
+            Material[] mats = renderer.materials;
+
+            mats[Mat_Nr] = Mat;
+
+            renderer.materials = mats;
         }
 
         #endregion
