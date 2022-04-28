4f12c791b697210bd6c82376c720a290fc59cba3
PedroAcevedo <pdacevedo@uninorte.edu.co>
2022-04-28 19:56:45 -0400

    Include Audio to DartSetGame

--- Assets/Scripts/SocialSpace/Dart/DartCollisionTable.cs
+++ Assets/Scripts/SocialSpace/Dart/DartCollisionTable.cs
@@ -7,11 +7,14 @@ using Photon.Realtime;
 public class DartCollisionTable : MonoBehaviour
 {
     public int scoreValue;
+    private AudioSource audEffect;
+    private UpdateUserInfo usrInfo;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        audEffect = this.GetComponent<AudioSource>();
+        usrInfo = GameObject.Find("XR Origin").GetComponent<UpdateUserInfo>();
     }
 
     // Update is called once per frame
@@ -24,9 +27,12 @@ public class DartCollisionTable : MonoBehaviour
     {
         if (other.tag == "Dart")
         {
+            audEffect.PlayOneShot(audEffect.clip);
             PhotonView pv = other.gameObject.GetComponent<PhotonView>();
             Debug.Log(pv.OwnerActorNr + " : Get Score " + scoreValue);
             Destroy(other.gameObject, 10);
+
+            usrInfo.addCoin();
         }
     }
 }
