3fa702052dd0f3e1eb94399b5a14a94379649ac8
mangalm2077 <mangalm@purdue.edu>
2022-04-27 20:31:50 -0400

    Completed

--- Assets/Scripts/PlayerNetworkSetup.cs
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -2,26 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class PlayerNetworkSetup : MonoBehaviourPunCallbacks
 {
     public GameObject localXRRigObject;
+    public GameObject MainAvatarGameObject;
 
     public GameObject AvatarHeadObject;
     public GameObject AvatarBodyObject;
+
+    public GameObject[] AvatarModelPrefabs;
+
+    public TMP_Text PlayerName;
     void Start()
     {
         if (photonView.IsMine)
         {
             localXRRigObject.SetActive(true);
+
+            object avatarSelectionNumber;
+
+            if(PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(CollabVRConstants.AVATAR_SELECTION_NUMBER, out avatarSelectionNumber))
+            {
+                Debug.Log("Selection num:" + (int)avatarSelectionNumber);
+                photonView.RPC("InitializeSelectedAvatarModel", RpcTarget.AllBuffered, (int)avatarSelectionNumber);
+            }
+
             SetLayerRecursively(AvatarHeadObject, 6);
             SetLayerRecursively(AvatarBodyObject, 7);
+
+            MainAvatarGameObject.AddComponent<AudioListener>();
         }
         else
         {
             localXRRigObject.SetActive(false);
             SetLayerRecursively(AvatarHeadObject, 0);
             SetLayerRecursively(AvatarBodyObject, 0);
+        }
+
+        if(PlayerName != null)
+        {
+            PlayerName.text = photonView.Owner.NickName;
         }
     }
 
@@ -37,5 +59,25 @@ public class PlayerNetworkSetup : MonoBehaviourPunCallbacks
         {
             trans.gameObject.layer = layerNumber;
         }
+    }
+
+    [PunRPC]
+    public void InitializeSelectedAvatarModel(int avatarSelectionNumber)
+    {
+        GameObject selectedAvatarGameobject = Instantiate(AvatarModelPrefabs[avatarSelectionNumber], localXRRigObject.transform);
+
+        AvatarInputConverter avatarInputConverter = localXRRigObject.GetComponent<AvatarInputConverter>();
+        AvatarHolder avatarHolder = selectedAvatarGameobject.GetComponent<AvatarHolder>();
+        SetUpAvatarGameobject(avatarHolder.HeadTransform, avatarInputConverter.AvatarHead);
+        SetUpAvatarGameobject(avatarHolder.BodyTransform, avatarInputConverter.AvatarBody);
+        SetUpAvatarGameobject(avatarHolder.HandLeftTransform, avatarInputConverter.AvatarHand_Left);
+        SetUpAvatarGameobject(avatarHolder.HandRightTransform, avatarInputConverter.AvatarHand_Right);
+    }
+
+    void SetUpAvatarGameobject(Transform avatarModelTransform, Transform mainAvatarTransform)
+    {
+        avatarModelTransform.SetParent(mainAvatarTransform);
+        avatarModelTransform.localPosition = Vector3.zero;
+        avatarModelTransform.localRotation = Quaternion.identity;
     }
 }
