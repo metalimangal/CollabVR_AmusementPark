ac60da1eb2e70bfdbb13a3c809843364a4afaa29
jaksiri <jakrinsiri@hotmail.com>
2022-05-01 17:04:29 -0400

    Updated Connect To server and network player prefab

--- Assets/Capture the flag/Scripts/CaptureTheFlagScripts/ConnectToServer.cs
+++ Assets/Capture the flag/Scripts/CaptureTheFlagScripts/ConnectToServer.cs
@@ -23,23 +23,6 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        Connect();
-    }
-
-    private void Connect()
-    {
-        PhotonNetwork.ConnectUsingSettings();
-        Debug.Log("Try connect to server...");
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Connected");
-        RoomOptions roomOptions = new RoomOptions();
-        roomOptions.IsOpen = true;
-        roomOptions.IsVisible = true;
-        roomOptions.MaxPlayers = 20;
-        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
     }
 
     public override void OnJoinedRoom()
@@ -66,32 +49,4 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
             CF_GameManager.Instance.UpdateGameState(GameState.Pregame);
         }
     }
-
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        //ReloadLevel();
-        base.OnPlayerEnteredRoom(newPlayer);
-    }
-
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        //ReloadLevel();
-        base.OnPlayerLeftRoom(otherPlayer);
-    }
-
-    public override void OnLeftRoom()
-    {
-        base.OnLeftRoom();
-        //PhotonNetwork.Destroy(spawnedPlayer);
-
-    }
-    private void ReloadLevel()
-    {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            Debug.Log("Reloading Level");
-            PhotonNetwork.LoadLevel(0);             // Might have to change this depending on the scene in the view;
-        }
-    }
-
 }
